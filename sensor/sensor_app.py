dc1ec018ab3577d321b8d425b028957e0677824b
Mario Gamboa <mgamboam@users.noreply.github.com>
2021-08-12 20:08:40 -0600

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -8,7 +8,6 @@ from datetime import date, datetime
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
-
 ##############################
 
 # Module 1 code here:
@@ -18,11 +17,11 @@ print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
-recs = house_info.get_data_by_area("id", test_area)
+recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
-recs = house_info.get_data_by_date("id", test_date)
+recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
